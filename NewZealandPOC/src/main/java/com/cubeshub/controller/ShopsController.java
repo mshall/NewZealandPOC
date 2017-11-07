6dfca50bb9eb0507f4e09702e2be6a97520d667a
mshall <mohamed.s.elshall2011@gmail.com>
2017-11-07 16:31:02 +0200

    modified the application.properties

--- NewZealandPOC/src/main/java/com/cubeshub/controller/ShopsController.java
+++ NewZealandPOC/src/main/java/com/cubeshub/controller/ShopsController.java
@@ -13,20 +13,23 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import com.cubeshub.dto.ShopsResponse;
 import com.cubeshub.service.ShopsService;
 
+import lombok.extern.slf4j.Slf4j;
+
 
 @EnableWebMvc
 @RestController
 @RequestMapping("/v1/")
+@Slf4j
 public class ShopsController {
 
-	Logger logger = LoggerFactory.getLogger(ShopsController.class);
+	
 	@Autowired
 	ShopsService service;
 
 	@RequestMapping(value = "shops", produces = "application/json; charset=UTF-8", method = RequestMethod.GET)
 	public ResponseEntity<ShopsResponse> findShopsByPostCode(@RequestParam(value = "town", required = true) String town,
 			@RequestParam(value = "postcode", required = false) String postcode) {
-		logger.info("Received a request to get shops for town" + town);
+		log.info("Received a request to get shops for town" + town);
 		ShopsResponse shops = service.getShopsByTown(town);
 		return new ResponseEntity<ShopsResponse>(shops, HttpStatus.OK);
 	}
